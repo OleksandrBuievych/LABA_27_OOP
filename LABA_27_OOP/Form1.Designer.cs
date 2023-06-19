ae0e82f673a490daafad69381dd5ef70f26b67a2
OleksandrBuievych <112573653+OleksandrBuievych@users.noreply.github.com>
2023-06-19 18:00:51 +0300

    Добавьте файлы проекта.

---
+++ LABA_27_OOP/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace LABA_27_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.propertiesTextBox = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Security = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(3, 3);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(808, 486);
+            this.treeView.TabIndex = 0;
+            this.treeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeExpand);
+            this.treeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_NodeMouseClick);
+            // 
+            // propertiesTextBox
+            // 
+            this.propertiesTextBox.Location = new System.Drawing.Point(682, 548);
+            this.propertiesTextBox.Name = "propertiesTextBox";
+            this.propertiesTextBox.Size = new System.Drawing.Size(784, 22);
+            this.propertiesTextBox.TabIndex = 3;
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(862, 13);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(604, 476);
+            this.listView.TabIndex = 5;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(484, 541);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Властивості";
+            // 
+            // Security
+            // 
+            this.Security.Location = new System.Drawing.Point(682, 607);
+            this.Security.Name = "Security";
+            this.Security.Size = new System.Drawing.Size(784, 22);
+            this.Security.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1492, 745);
+            this.Controls.Add(this.Security);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.propertiesTextBox);
+            this.Controls.Add(this.treeView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.TextBox propertiesTextBox;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Security;
+    }
+}
+
---
+++ LABA_27_OOP/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LABA_27_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDrives()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                TreeNode driveNode = new TreeNode(drive.Name);
+                driveNode.Tag = drive.RootDirectory;
+                driveNode.Nodes.Add(new TreeNode()); // Add a dummy node to show expand icon
+
+                treeView.Nodes.Add(driveNode);
+            }
+        }
+
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode selectedNode = e.Node;
+
+            if (selectedNode.Nodes.Count == 1 && selectedNode.Nodes[0].Text == "")
+            {
+                selectedNode.Nodes.Clear();
+                DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+
+                try
+                {
+                    foreach (DirectoryInfo subDir in selectedDir.GetDirectories())
+                    {
+                        TreeNode subDirNode = new TreeNode(subDir.Name);
+                        subDirNode.Tag = subDir;
+                        subDirNode.Nodes.Add(new TreeNode()); // Add a dummy node to show expand icon
+
+                        selectedNode.Nodes.Add(subDirNode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void treeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode selectedNode = e.Node;
+            DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+
+            listView.Items.Clear();
+
+            try
+            {
+                foreach (DirectoryInfo subDir in selectedDir.GetDirectories())
+                {
+                    ListViewItem item = new ListViewItem(subDir.Name);
+                    item.SubItems.Add("Folder");
+                    item.SubItems.Add(subDir.LastWriteTime.ToString());
+
+                    listView.Items.Add(item);
+                }
+
+                foreach (FileInfo file in selectedDir.GetFiles())
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add("File");
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+
+                    listView.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void listView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView.SelectedItems[0];
+                string selectedItemText = selectedItem.Text;
+                TreeNode selectedNode = treeView.SelectedNode;
+                DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+                string path = Path.Combine(selectedDir.FullName, selectedItemText);
+
+                if (selectedItem.SubItems[1].Text == "Folder")
+                {
+                    DirectoryInfo selectedDirectory = new DirectoryInfo(path);
+                    ShowDirectoryContents(selectedDirectory);
+                    DisplaySecurityAttributes(selectedDirectory);
+                }
+                else if (selectedItem.SubItems[1].Text == "File")
+                {
+                    FileInfo selectedFile = new FileInfo(path);
+                    ShowFileProperties(selectedFile);
+                    DisplaySecurityAttributes(selectedFile);
+                }
+            }
+        }
+
+        private void ShowFileProperties(FileInfo file)
+        {
+            propertiesTextBox.Text = $"File Name: {file.Name}\t";
+            propertiesTextBox.Text += $"Size: {file.Length} bytes\r\n";
+        }
+
+        private void ShowDirectoryContents(DirectoryInfo directory)
+        {
+            listView.Items.Clear();
+
+            try
+            {
+                foreach (DirectoryInfo subDir in directory.GetDirectories())
+                {
+                    ListViewItem item = new ListViewItem(subDir.Name);
+                    item.SubItems.Add("Folder");
+                    item.SubItems.Add(subDir.LastWriteTime.ToString());
+
+                    listView.Items.Add(item);
+                }
+
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add("File");
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+
+                    listView.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void DisplaySecurityAttributes(FileSystemInfo fileSystemInfo)
+        {
+            try
+            {
+                var securityAttributes = File.GetAccessControl(fileSystemInfo.FullName);
+                var securityRules = securityAttributes.GetAccessRules(true, true, typeof(System.Security.Principal.SecurityIdentifier));
+                var securityInfo = new StringBuilder();
+
+                foreach (FileSystemAccessRule rule in securityRules)
+                {
+                    securityInfo.AppendLine($"Ідентифікатор: {rule.IdentityReference}");
+                    securityInfo.AppendLine($"Тип: {rule.AccessControlType}");
+                    securityInfo.AppendLine($"Права: {rule.FileSystemRights}");
+                    securityInfo.AppendLine();
+                }
+
+                Security.Text = "Атрибути безпеки: \r\n";
+                Security.Text += securityInfo.ToString().Replace("\r\n", "\r\n ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка відображення атрибутів безпеки: " + ex.Message);
+            }
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDrives();
+        }
+    }
+}
