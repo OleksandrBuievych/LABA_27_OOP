2dd7b497abff8d44d28bf62ffd759e2cf4e40d22
OleksandrBuievych <112573653+OleksandrBuievych@users.noreply.github.com>
2023-06-24 15:48:38 +0300

    Лаба 28

--- LABA_27_OOP/Form1.Designer.cs
+++ LABA_27_OOP/Form1.Designer.cs
@@ -34,6 +34,12 @@ namespace LABA_27_OOP
             this.listView = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
             this.Security = new System.Windows.Forms.TextBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnCreateArchive = new System.Windows.Forms.Button();
+            this.btnExtractArchive = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // treeView
@@ -79,11 +85,77 @@ namespace LABA_27_OOP
             this.Security.Size = new System.Drawing.Size(784, 22);
             this.Security.TabIndex = 7;
             // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(100, 607);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(104, 23);
+            this.btnMove.TabIndex = 8;
+            this.btnMove.Text = "Перемістити";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(266, 606);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(102, 53);
+            this.btnCreate.TabIndex = 9;
+            this.btnCreate.Text = "Створити файл";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(104, 541);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Видалити";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(274, 541);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(94, 23);
+            this.btnCopy.TabIndex = 11;
+            this.btnCopy.Text = "Копіювати";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnCreateArchive
+            // 
+            this.btnCreateArchive.Location = new System.Drawing.Point(489, 663);
+            this.btnCreateArchive.Name = "btnCreateArchive";
+            this.btnCreateArchive.Size = new System.Drawing.Size(120, 23);
+            this.btnCreateArchive.TabIndex = 12;
+            this.btnCreateArchive.Text = "Архівувати";
+            this.btnCreateArchive.UseVisualStyleBackColor = true;
+            this.btnCreateArchive.Click += new System.EventHandler(this.btnCreateArchive_Click);
+            // 
+            // btnExtractArchive
+            // 
+            this.btnExtractArchive.Location = new System.Drawing.Point(615, 663);
+            this.btnExtractArchive.Name = "btnExtractArchive";
+            this.btnExtractArchive.Size = new System.Drawing.Size(122, 23);
+            this.btnExtractArchive.TabIndex = 13;
+            this.btnExtractArchive.Text = "Розпакувати";
+            this.btnExtractArchive.UseVisualStyleBackColor = true;
+            this.btnExtractArchive.Click += new System.EventHandler(this.btnExtractArchive_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1492, 745);
+            this.Controls.Add(this.btnExtractArchive);
+            this.Controls.Add(this.btnCreateArchive);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnMove);
             this.Controls.Add(this.Security);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView);
@@ -104,6 +176,12 @@ namespace LABA_27_OOP
         private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox Security;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnCreateArchive;
+        private System.Windows.Forms.Button btnExtractArchive;
     }
 }
 
--- LABA_27_OOP/Form1.cs
+++ LABA_27_OOP/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
@@ -116,6 +118,21 @@ namespace LABA_27_OOP
                     DisplaySecurityAttributes(selectedFile);
                 }
             }
+
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView.SelectedItems[0];
+                string selectedItemText = selectedItem.Text;
+                TreeNode selectedNode = treeView.SelectedNode;
+                DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+                string filePath = Path.Combine(selectedDir.FullName, selectedItemText);
+
+                if (Path.GetExtension(filePath) == ".txt")
+                {
+                    // Відкрити файл у блокноті
+                    Process.Start("notepad.exe", filePath);
+                }
+            }
         }
 
         private void ShowFileProperties(FileInfo file)
@@ -179,12 +196,205 @@ namespace LABA_27_OOP
             }
         }
 
+        private void CreateFile(string path)
+        {
+            try
+            {
+                File.Create(path);
+                ShowDirectoryContents((DirectoryInfo)treeView.SelectedNode.Tag);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка створення файлу: " + ex.Message);
+            }
+        }
+
+        private void MoveFile(string sourcePath, string destinationPath)
+        {
+            try
+            {
+                File.Move(sourcePath, destinationPath);
+                ShowDirectoryContents((DirectoryInfo)treeView.SelectedNode.Tag);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка переміщення файлу: " + ex.Message);
+            }
+        }
+
+        private void CopyFile(string sourcePath, string destinationPath)
+        {
+            try
+            {
+                File.Copy(sourcePath, destinationPath);
+                ShowDirectoryContents((DirectoryInfo)treeView.SelectedNode.Tag);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка копіювання файлу: " + ex.Message);
+            }
+        }
+
+        private void DeleteFile(string path)
+        {
+            try
+            {
+                File.Delete(path);
+                ShowDirectoryContents((DirectoryInfo)treeView.SelectedNode.Tag);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка видалення файлу: " + ex.Message);
+            }
+        }
+
+
+
 
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
             LoadDrives();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView.SelectedItems[0];
+                string selectedItemText = selectedItem.Text;
+                TreeNode selectedNode = treeView.SelectedNode;
+                DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+                string filePath = Path.Combine(selectedDir.FullName, selectedItemText);
+
+                DialogResult result = MessageBox.Show("Ви дійсно бажаєте видалити файл?", "Видалення файлу", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    DeleteFile(filePath);
+                }
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView.SelectedItems[0];
+                string selectedItemText = selectedItem.Text;
+                TreeNode selectedNode = treeView.SelectedNode;
+                DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+                string sourcePath = Path.Combine(selectedDir.FullName, selectedItemText);
+
+                using (var dialog = new SaveFileDialog())
+                {
+                    dialog.Title = "Виберіть місце для копіювання файлу";
+                    dialog.FileName = selectedItemText;
+
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string destinationPath = dialog.FileName;
+                        CopyFile(sourcePath, destinationPath);
+                    }
+                }
+            }
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView.SelectedItems[0];
+                string selectedItemText = selectedItem.Text;
+                TreeNode selectedNode = treeView.SelectedNode;
+                DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+                string sourcePath = Path.Combine(selectedDir.FullName, selectedItemText);
+
+                using (var dialog = new SaveFileDialog())
+                {
+                    dialog.Title = "Виберіть місце для переміщення файлу";
+                    dialog.FileName = selectedItemText;
+
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string destinationPath = dialog.FileName;
+                        MoveFile(sourcePath, destinationPath);
+                    }
+                }
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = treeView.SelectedNode;
+            DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+
+            using (var dialog = new SaveFileDialog())
+            {
+                dialog.Title = "Виберіть місце для створення файлу";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = dialog.FileName;
+                    CreateFile(filePath);
+                }
+            }
+        }
+
+        private void CreateZipArchive(string sourceDirectory, string destinationPath)
+        {
+            ZipFile.CreateFromDirectory(sourceDirectory, destinationPath);
+        }
+
+        // Метод для розпакування ZIP-архіву у вказаний каталог
+        private void ExtractZipArchive(string sourceFilePath, string destinationDirectory)
+        {
+            ZipFile.ExtractToDirectory(sourceFilePath, destinationDirectory);
+        }
+
+        // Обробник події для кнопки "Створити архів"
+        private void btnCreateArchive_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = treeView.SelectedNode;
+            DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+
+            using (var dialog = new SaveFileDialog())
+            {
+                dialog.Title = "Виберіть місце для створення ZIP-архіву";
+                dialog.DefaultExt = "zip";
+                dialog.Filter = "ZIP Files (*.zip)|*.zip";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string archivePath = dialog.FileName;
+                    CreateZipArchive(selectedDir.FullName, archivePath);
+                }
+            }
+        }
+
+        // Обробник події для кнопки "Розпакувати архів"
+        private void btnExtractArchive_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView.SelectedItems[0];
+                string selectedItemText = selectedItem.Text;
+                TreeNode selectedNode = treeView.SelectedNode;
+                DirectoryInfo selectedDir = (DirectoryInfo)selectedNode.Tag;
+                string archivePath = Path.Combine(selectedDir.FullName, selectedItemText);
+
+                using (var dialog = new FolderBrowserDialog())
+                {
+                    dialog.Description = "Виберіть каталог для розпакування ZIP-архіву";
+
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string destinationDirectory = dialog.SelectedPath;
+                        ExtractZipArchive(archivePath, destinationDirectory);
+                    }
+                }
+            }
         }
     }
 }
